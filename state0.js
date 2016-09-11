cb79cbf3c28dfbf9cc8924148f534507e95e3924
DayNorth <andrey.flores.94@gmail.com>
2016-09-10 18:26:06 -0600

    add animations, some physics and the state 0

--- state0.js
+++ state0.js
@@ -1,11 +1,15 @@
-var demo = {}, altura  = 1920/2, ancho = 1080/2, larry, speed = 4;
+var demo = {}, larry, altura = 1500/2, ancho= 1000/2, speed = 5, lar;
 demo.state0 = function(){};
 demo.state0.prototype = {
     preload: function (){
-        game.load.image('larry', 'assets/sprites/larry1.png');
+        game.load.spritesheet('lar', 'assets/sprites/larry.png', 85, 110, 7);
+        game.load.image('terreno', 'assets/sprites/background1.png');
 
     },
     create: function(){
+        //adding physics engine, so we could add some physics to the game such as not leaving the map
+        //start the physics engine.
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         game.stage.backgroundColor = '#80ff80';
         console.log('state0');
         //THINGS TO REMEMBER
@@ -14,26 +18,63 @@ demo.state0.prototype = {
         //we just call the function that does all the callbacks
         addChangeStateEventListeners();
         
-        //to scale the size of the game in relation to the window's size
+        game.world.setBounds(0,0,2000, 1000);
+        //scale the size of the game in relation to the window's size
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+        var terreno = game.add.sprite(0, 0, 'terreno');
+        //adjust the image to the screen
+        terreno.scale.setTo(1,1.1);
+ 
+        lar = game.add.sprite(altura, game.world.height - 250,'lar');
         
+        // to make him look the left side
+        lar.scale.setTo(-1, 1);
         
-        //add image to the map
-        larry = game.add.sprite(altura, ancho, 'larry');
-        larry.anchor.setTo(0.5,0.5);
+        //adding physics to a specific object
+        game.physics.enable(lar);
+        
+        //making him bounds with the map world
+        lar.body.collideWorldBounds = true;
+        lar.animations.add('walk',[0,1,2,5,6,7]); //  utiliza los frames 1,2,3 para correr a la izquierda ,10 fps, true le dice que haga un loop
+        //adding camera movement
+        game.camera.follow(lar);
+        //adding a rectangule so when the character moves away from the rectangule, the camera starts to follow him
+        game.camera.deadzone = new Phaser.Rectangule(ancho - 300, 0, 600, 1000);
+        
+        //animation for walking
+
+
     },
     update: function(){
         if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
-            larry.x += speed;
+            lar.scale.setTo(1, 1);
+            lar.animations.play('walk', 5, true);
+            lar.x += speed;
         }
         else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
-            larry.x -= speed;
+            lar.scale.setTo(-1, 1);
+            lar.x -= speed;
+            lar.animations.play('walk', 5, true);
+        } else {
+            //stops the animations once he has stopped walking
+            lar.animations.stop('walk');
+            //sets the character to the intial frame
+            lar.frame = 0;
         }
         if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
-            larry.y  -= speed;
+            lar.y  -= speed;
+            //to keep him on the sidewalk
+            if(lar.y < 710){
+                lar.y = 710;
+            }
         }
         else if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
-            larry.y += speed;
+            lar.y += speed;
+            //to keep him on the sidewalk
+            if(lar.y > 940){
+                lar.y = 940;
+            }
         }
     }
     
